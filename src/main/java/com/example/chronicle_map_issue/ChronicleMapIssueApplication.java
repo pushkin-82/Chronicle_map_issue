d51267b0501bdac895c53303ad55f0136d5daa3c
Serhii Putylin <serhii.putylin@blockpit.io>
2023-08-15 20:43:16 +0300

    initial commit

---
+++ src/main/java/com/example/chronicle_map_issue/ChronicleMapIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.chronicle_map_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronicleMapIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronicleMapIssueApplication.class, args);
+	}
+
+}
